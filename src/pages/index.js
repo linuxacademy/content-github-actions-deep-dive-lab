14a5c47685eef99f04737ebecdf4e562effed33f
JonnyOrtiz-source <j0nny.0rt1z@yahoo.com>
2021-10-19 17:26:37 -0400

    added launch date in h4 w/o exclamation

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ const IndexPage = () => {
                     Welcome to <code className="title_code">Cloud Coffee</code>
                 </h1>
 
-                <h4 className="title">Launch date announced!</h4>
+                <h4 className="title">Launch date announced</h4>
 
                 <p className="description">
                     Join Our Mailing List!
