ba1dc0e406ff0391042c3a7634b41f300ff4117c
JonnyOrtiz-source <j0nny.0rt1z@yahoo.com>
2021-10-19 17:19:42 -0400

    added launch date in h4

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,49 +1,51 @@
-import * as React from "react"
-import { StaticImage } from "gatsby-plugin-image"
-import "../styles/global.css"
+import * as React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import '../styles/global.css';
 
-import Layout from '../components/layout'
+import Layout from '../components/layout';
 
 const IndexPage = () => {
-  return (
-    <Layout>
-      <header>
-        <title>Cloud Coffee</title>
-        <meta name="description" content="Bringing Coffee to the Cloud" />
-      </header>
-      
-      <main className='main'>
-        
+    return (
+        <Layout>
+            <header>
+                <title>Cloud Coffee</title>
+                <meta
+                    name="description"
+                    content="Bringing Coffee to the Cloud"
+                />
+            </header>
 
-          <h1 className='title'>
-            Welcome to{' '}
-            <code className='title_code'>Cloud Coffee</code>
-          </h1>
-      
+            <main className="main">
+                <h1 className="title">
+                    Welcome to <code className="title_code">Cloud Coffee</code>
+                </h1>
 
-          <p className='description'>
-            Join Our Mailing List!
-            <br/>
-            <input type="text"></input>
-            <br/>
-            <button>Submit</button>
-          </p>
+                <h4 className="title">Launch date announced!</h4>
 
-          <footer className='footer'>
-            <a
-              href="https://acloud.guru"
-            >
-              Powered by{' '}
-              <span className='logo'>
-                <StaticImage src="../images/ACG_Orange_Logo_Horizontal.png" alt="A Cloud Guru logo" width={119} height={16} />
-              </span>
-            </a>
-          </footer>
-      </main>
+                <p className="description">
+                    Join Our Mailing List!
+                    <br />
+                    <input type="text"></input>
+                    <br />
+                    <button>Submit</button>
+                </p>
 
-      
-    </Layout>
-  )
-}
+                <footer className="footer">
+                    <a href="https://acloud.guru">
+                        Powered by{' '}
+                        <span className="logo">
+                            <StaticImage
+                                src="../images/ACG_Orange_Logo_Horizontal.png"
+                                alt="A Cloud Guru logo"
+                                width={119}
+                                height={16}
+                            />
+                        </span>
+                    </a>
+                </footer>
+            </main>
+        </Layout>
+    );
+};
 
-export default IndexPage
+export default IndexPage;
