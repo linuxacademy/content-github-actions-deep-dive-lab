32e7852f0ba6e3df322ffb5d3041b33aac5cf016
Michael Ly <105381067+magile32@users.noreply.github.com>
2023-03-09 15:41:12 -0800

    Update index.js (#2)

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,8 @@ const IndexPage = () => {
             Welcome to{' '}
             <code className='title_code'>Cloud Coffee</code>
           </h1>
-      
+
+          <h4 className='title'>Launch Date Announced!!!</h4>
 
           <p className='description'>
             Join Our Mailing List!
